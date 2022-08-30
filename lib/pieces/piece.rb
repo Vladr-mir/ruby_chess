fe4d681c71a72337907a4ce5e81554c26bada462
Vladimir <duenasvladimir@gmail.com>
2022-08-29 21:39:30 -0600

    Add basic functionality to Piece class

---
+++ lib/pieces/piece.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: false
+
+# Blueprint for chess pieces
+class Piece
+  attr_reader :position, :total_moves, :notation
+  attr_accessor :is_threatened
+
+  def initialize(symbol, notation, position)
+    @symbol = symbol
+    @notation = notation
+    @position = position
+    @total_moves = 0
+    @is_threatened = false
+  end
+
+  def to_s
+    @symbol
+  end
+
+  def to_notation
+    "#{@notation}#{position_to_notation}"
+  end
+
+  private
+
+  def position_to_notation
+    columns = %w[a b c d e f g h]
+    "#{columns[@position[0]]}#{@position[1] + 1}"
+  end
+
+  def moveset
+    total_moves.zero? ? [[1, 0], [2, 0]] : [[1, 0]]
+  end
+
+  def attackset
+    [[1, 1], [1, -1]]
+  end
+end
