dcccf87d4f426414a0a33f592eaf70da0f5c8106
Vladimir <duenasvladimir@gmail.com>
2022-09-08 18:32:09 -0600

    Add testcase for valid_move? method

--- spec/pieces/piece_spec.rb
+++ spec/pieces/piece_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Piece do
       expect(piece2.to_notation).to eql('Pd4')
     end
   end
+
+  describe '#valid_move?' do
+    it 'uses the moveset correctly' do
+      piece = Piece.new('', 'P', [0, 0])
+
+      expect(piece.valid_move?([1, 0])).to eql(true)
+    end
+  end
 end
