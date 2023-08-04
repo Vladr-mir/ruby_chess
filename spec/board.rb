08a4ace7ad75b0668b5b836026d656ebaf3950ac
Vladimir <einederemehr@gmail.com>
2023-08-03 20:53:15 -0600

    Add test cases to board.rb file

---
+++ spec/board.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: false
+
+require "./lib/board"
+
+# frozen_string_literal: true
+
+describe do
+  describe "#board" do
+    it "returns an empty grid" do
+      data = Array.new(8) { Array.new(8) }
+      board = ChessBoard.new(8)
+      expect(data).to eq(board.board)
+    end
+
+    it "returns symbol at pos_x pos_y" do
+      board = ChessBoard.new(8)
+
+      board.place_symbol([3, 4], "x")
+      expect(board.board[3][4]).to eq("x")
+    end
+  end
+end
