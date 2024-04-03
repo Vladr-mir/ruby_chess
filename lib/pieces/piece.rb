e08dbc1fc87d509063b16cb2c39bcadccf65f6be
Vladimir <duenasvladimir@gmail.com>
2024-04-02 19:22:16 -0600

    Add extended_moveset

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -5,8 +5,9 @@ class Piece
   attr_reader :symbol
   attr_accessor :pos
 
-  def initialize(symbol = "♙", pos = [0, 0], board_size = 8)
-    @board_size = board_size
+  BOARD_SIZE = 8
+
+  def initialize(pos, symbol = "♙")
     @symbol = symbol
     @pos = pos
   end
@@ -15,27 +16,48 @@ class Piece
     @symbol
   end
 
-  def valid_move?
-    # Find adjacent moves using movement range
-    adjacents_moves = []
-    movement_range.map do |move|
-      # sums movement range and position
-      adjacents_moves << [pos, move].transpose.map(&:sum)
-    end
+  def valid_direction?(move)
+    return false if out_of_bound?(move)
 
-    # remove invalid moves
-    adjacents_moves.select do |move|
-      move =move.select { |direction| direction.between?(0, @board_size - 1) }
-      move.length >= 2
-    end
+    !get_direction(move).empty?
   end
 
-  def movement_range
+  def moveset
     [
-      [0, 1],
-      [0, -1],
       [1, 0],
-      [-1, 0]
+      [0, 1],
+      [-1, 0],
+      [0, -1]
     ]
+  end
+
+  # private
+
+  def valid_move_extended?(move)
+    range_move = @pos.clone
+    direction = find_relative_move(move)
+    direction = direction.map { |axis| axis <=> 0 }
+    return false unless moveset.any?(direction)
+
+    until range_move == move || out_of_bound?(range_move)
+      range_move[0] += direction[0]
+      range_move[1] += direction[1]
+      p range_move
+    end
+
+    range_move == move
+  end
+
+  def get_direction(move)
+    relative_move = find_relative_move(move)
+    moveset.select { |direction| direction == relative_move }.flatten
+  end
+
+  def find_relative_move(move)
+    move.zip(@pos).map { |pos| pos.inject(:-) }
+  end
+
+  def out_of_bound?(pos)
+    pos.count { |axis| axis >= BOARD_SIZE - 1 }.positive?
   end
 end
