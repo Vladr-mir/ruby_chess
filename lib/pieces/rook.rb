7ec363d319034f3a0c235864c55024d95134eeec
Vladimir <duenasvladimir@gmail.com>
2024-04-04 19:20:22 -0600

    Added reader accesor to is_white in Rook class

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -4,8 +4,11 @@ require_relative "piece"
 
 # Rook piece
 class Rook < Piece
+  attr_reader :is_white
+
   def initialize(pos, is_white)
-    symbol = is_white ? "♖" : "♜"
+    @is_white = is_white
+    symbol = @is_white ? "♖" : "♜"
     super(pos, true, symbol)
   end
 end
